e6e1332726fc4dc53d9f26f614f0c5f4536997db
Tiruptirrr <kaiputirupatireddy7013@gmail.com>
2025-02-18 09:38:52 +0530

    changed

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let http=require("http")
 const server=http.createServer((req,res)=>{
-    res.write("Hello ra karthik");
+    res.write("Hello ra hai namaskaram  ");
     res.end("response ended");
 })
 server.listen("3002",()=>console.log("server running"))
