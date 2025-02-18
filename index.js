3e3c0dac19b2975ffc04c01ca5cf71f3f760ac37
Tiruptirrr <kaiputirupatireddy7013@gmail.com>
2025-02-18 15:26:26 +0530

    new messege

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let http=require("http")
 const server=http.createServer((req,res)=>{
-    res.write("Hello ra hai namaskaram  ");
+    res.write("Andharu Bagundaali anduloo nen undaali....  ");
     res.end("response ended");
 })
 server.listen("3002",()=>console.log("server running"))
